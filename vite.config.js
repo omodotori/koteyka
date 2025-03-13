9ced222c3865f701d4fc8837016464666129014e
Amina <aktaevaamina07@gmail.com>
2025-03-13 18:04:42 +0500

    fixing deploy x3

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,21 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+
+  esbuild: {
+    loader: "jsx",
+  },
+
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      }
+    }
+  }
 });
